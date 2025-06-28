d8b51cba1c15e39869b43cb37b764c0e68be3c95
AmitJavaCoding <93794474+missioncode7@users.noreply.github.com>
2025-06-28 18:09:41 +0530

    Add files via upload
    commit

---
+++ dockerize_first_app/dockerize_first_app/src/main/java/com/dockerize_app/DockerizeFirstAppApplication.java
@@ -0,0 +1,13 @@
+package com.dockerize_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizeFirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizeFirstAppApplication.class, args);
+	}
+
+}
---
+++ dockerize_first_app/dockerize_first_app/src/test/java/com/dockerize_app/DockerizeFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockerize_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerizeFirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
